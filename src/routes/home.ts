72f8381fedd10c033d33f74203828e6dd3edc62c
kk-mats <k.mats.kk@gmail.com>
2019-11-28 14:56:46 +0900

    feat: Remove temporal routes

--- src/routes/home.ts
+++
@@ -1,9 +0,0 @@
-import * as express from "express";
-
-const router = express.Router();
-
-router.get<{ uid: string }>("/", (req, res) => {
-	res.send({ uid: req.params.uid, message: "authed" });
-});
-
-export default router;
