955597d5b1fa1c7a9dad2fa98b5bfddb5451ea89
kk-mats <k.mats.kk@gmail.com>
2019-11-28 14:54:02 +0900

    feat: Add repositories of Job and Project

---
+++ src/repository/JobRepository.ts
@@ -0,0 +1,85 @@
+import * as mongoose from "mongoose";
+
+import * as Job from "src/models/Job";
+import * as Failable from "src/types/failure/Failable";
+import FailureCode from "src/types/failure/FailureCode";
+
+import DetectionQuery from "src/types/query/DetectionQuery";
+
+class Type {
+	public static async push(
+		ownerId: mongoose.Types.ObjectId,
+		projectSlug: string,
+		hid: mongoose.Types.ObjectId,
+		detector: { slug: string; version: string },
+		query: DetectionQuery
+	): Promise<Job.Type> {
+		return Job.Model.create({
+			ownerId,
+			projectSlug,
+			hid,
+			scheduled: new Date(),
+			detector,
+			query
+		});
+	}
+
+	public static async first(
+		slug: string,
+		versions: string[]
+	): Promise<Failable.Type<Job.Type>> {
+		const j = await Job.Model.findOne(
+			{
+				"detector.slug": slug,
+				"detector.version": { $in: versions }
+			},
+			null,
+			{
+				sort: { scheduled: 1 }
+			}
+		).exec();
+
+		if (j) {
+			return Failable.succeed(j);
+		}
+		return Failable.fail(
+			FailureCode.project.jobUnavailable(slug, versions)
+		);
+	}
+
+	public static async dispatch(
+		jid: mongoose.Types.ObjectId
+	): Promise<Failable.Type<Job.Type>> {
+		return this.remove(jid);
+	}
+
+	public static async remove(
+		jid: mongoose.Types.ObjectId
+	): Promise<Failable.Type<Job.Type>> {
+		const j = await Job.Model.findByIdAndRemove(jid);
+		if (j) {
+			return Failable.succeed(j);
+		}
+		return Failable.fail(
+			FailureCode.project.jobNotFound(jid.toHexString())
+		);
+	}
+
+	public static async finished(
+		hid: mongoose.Types.ObjectId
+	): Promise<Failable.Type<Job.Type>> {
+		const j = await Job.Model.findByIdAndUpdate(
+			{ hid },
+			{ dispatched: new Date() }
+		);
+
+		if (j) {
+			return Failable.succeed(j);
+		}
+		return Failable.fail(
+			FailureCode.project.jobNotFound(hid.toHexString())
+		);
+	}
+}
+
+export default Type;
--- src/repository/ProjectRepository.ts
+++ src/repository/ProjectRepository.ts
@@ -1,17 +1,316 @@
+import { promises as fs } from "fs";
 import * as rimraf from "rimraf";
 import * as path from "path";
 import { promisify } from "util";
 
+import * as mongoose from "mongoose";
+import * as simplegit from "simple-git/promise";
+import slugify from "slugify";
+
+import * as pathUtils from "src/util";
+
 import * as Failable from "src/types/failure/Failable";
 import FailureCode from "src/types/failure/FailureCode";
+import * as ProjectComm from "src/types/comm/Project";
+
+import * as Project from "src/models/Project";
+import * as History from "src/models/History";
 
 import ConfigRepository from "src/repository/ConfigRepository";
+import VCS from "src/types/VCS";
+import Status from "src/types/Status";
+import Detector from "src/types/Detector";
+import Target from "src/types/query/Target";
 
-class Type {
-	readonly project = ConfigRepository.paths.projects;
+export class Type {
+	public static async findByOwnerId(
+		ownerId: mongoose.Types.ObjectId
+	): Promise<Failable.Type<Project.Type[]>> {
+		return Failable.succeed(await Project.Model.find({ ownerId }).exec());
+	}
 
-	public async deleteProject(name: string): Promise<Failable.Type<string>> {
-		const p = path.resolve(this.project, name);
+	public static async findBySlug(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string
+	): Promise<Failable.Type<Project.Type>> {
+		const r = await Project.Model.findOne({ ownerId, slug }).exec();
+		if (r) {
+			return Failable.succeed(r);
+		}
+		return Failable.fail(FailureCode.project.slugNotFound(slug));
+	}
+
+	public static async resolveTarget(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string,
+		target: Target
+	): Promise<Failable.Type<{ targetDir: string; historiesDir: string }>> {
+		const p = await this.findBySlug(ownerId, slug);
+
+		if (p.failure) {
+			return Failable.delegate(p);
+		}
+
+		const { codes, histories } = pathUtils.resolveChildrenPath(
+			ownerId,
+			p.value._id
+		);
+		const targetDir = path.resolve(codes, target.targetDir || "");
+
+		const stat = await fs.stat(targetDir);
+		if (stat.isDirectory || stat.isFile) {
+			return Failable.succeed({
+				targetDir,
+				historiesDir: histories
+			});
+		}
+		return Failable.fail(FailureCode.project.invalidTarget(slug, target));
+	}
+
+	public static async getBranches(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string
+	): Promise<Failable.Type<string[]>> {
+		const r = await this.findBySlug(ownerId, slug);
+		if (r.failure) {
+			return Failable.delegate(r);
+		}
+		if (r.value.vcs === "Unversioned") {
+			return Failable.fail(FailureCode.project.unversioned(slug));
+		}
+		const git = simplegit(
+			pathUtils.resolveChildrenPath(ownerId, r.value._id).codes
+		);
+		return Failable.succeed((await git.branch([])).all);
+	}
+
+	public static async getHistories(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string
+	): Promise<Failable.Type<History.Type[]>> {
+		const r = await this.findBySlug(ownerId, slug);
+		if (r.failure) {
+			return Failable.delegate(r);
+		}
+		return Failable.succeed(r.value.histories);
+	}
+
+	public static async findHistory(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string,
+		hid: string
+	): Promise<
+		Failable.Type<{ project: Project.Type; history: History.Type }>
+	> {
+		const p = await this.findBySlug(ownerId, slug);
+		if (p.failure) {
+			return Failable.delegate(p);
+		}
+
+		const h = p.value.histories.find(value => {
+			return value._id.toHexString() === hid;
+		});
+
+		if (!h) {
+			return Failable.fail(
+				FailureCode.project.historyNotFound(slug, hid)
+			);
+		}
+
+		return Failable.succeed({
+			project: p.value,
+			history: h
+		});
+	}
+
+	private static async create(
+		ownerId: mongoose.Types.ObjectId,
+		name: string,
+		vcs: VCS,
+		loader: (codesDir: string, artifactsDir: string) => Promise<void>
+	): Promise<Failable.Type<Project.Type>> {
+		try {
+			const p = await Project.Model.create({
+				ownerId,
+				name,
+				slug: slugify(name),
+				vcs,
+				histories: []
+			});
+
+			const { codes, histories } = pathUtils.resolveChildrenPath(
+				ownerId,
+				p._id
+			);
+
+			await fs.mkdir(codes, { recursive: true });
+			await Promise.all([
+				fs.mkdir(histories, { recursive: true }),
+				loader(codes, histories)
+			]);
+
+			return Failable.succeed(p);
+		} catch (err) {
+			if (err.code && err.code === 11000) {
+				return Failable.fail(
+					FailureCode.project.slugAlreadyExists(name)
+				);
+			}
+			return Failable.fail(
+				FailureCode.common.unknown("ProjectRepository::create", err)
+			);
+		}
+	}
+
+	public static async delete(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string
+	): Promise<Failable.Type<boolean>> {
+		const p = await Project.Model.findOneAndDelete({
+			ownerId,
+			slug
+		}).exec();
+		if (p) {
+			await promisify(rimraf)(
+				pathUtils.resolveProjectPath(ownerId, p._id)
+			);
+			return Failable.succeed(true);
+		}
+		return Failable.fail(FailureCode.project.slugNotFound(slug));
+	}
+
+	public static async createHistory(
+		ownerId: mongoose.Types.ObjectId,
+		slug: string,
+		detector: Detector,
+		query: {
+			target: string;
+			parameters: object;
+		}
+	): Promise<
+		Failable.Type<{ hid: mongoose.Types.ObjectId; historyDir: string }>
+	> {
+		const p = await Project.Model.findOneAndUpdate(
+			{ ownerId, slug },
+			{
+				$push: {
+					histories: {
+						created: new Date(),
+						detector,
+						query: {
+							target: query.target,
+							parameters: query.parameters
+						},
+						status: "Pending"
+					}
+				}
+			},
+			{ new: true }
+		).exec();
+
+		if (!p) {
+			return Failable.fail(FailureCode.project.slugNotFound(slug));
+		}
+		const hid = p.histories[p.histories.length - 1]._id;
+		const historyDir = path.resolve(
+			pathUtils.resolveChildrenPath(ownerId, p._id).histories,
+			hid.toHexString(),
+			"artifacts"
+		);
+		await fs.mkdir(historyDir, { recursive: true });
+
+		return Failable.succeed({
+			hid,
+			historyDir
+		});
+	}
+
+	private static async updateHistoryStatus(
+		ownerId: mongoose.Types.ObjectId,
+		projectSlug: string,
+		historyId: mongoose.Types.ObjectId,
+		status: Status
+	): Promise<Failable.Type<boolean>> {
+		const p = await Project.Model.findOneAndUpdate(
+			{ ownerId, slug: projectSlug, "histories._id": historyId },
+			{
+				$set: {
+					"histories.$.dispatched": new Date(),
+					"histories.$.status": status
+				}
+			}
+		).exec();
+
+		if (p) {
+			return Failable.succeed(true);
+		}
+
+		return Failable.fail(
+			FailureCode.project.historyNotFound(
+				projectSlug,
+				historyId.toHexString()
+			)
+		);
+	}
+
+	public static async dispatch(
+		ownerId: mongoose.Types.ObjectId,
+		projectSlug: string,
+		historyId: mongoose.Types.ObjectId
+	): Promise<Failable.Type<boolean>> {
+		return this.updateHistoryStatus(
+			ownerId,
+			projectSlug,
+			historyId,
+			"Running"
+		);
+	}
+
+	public static async succeeded(
+		ownerId: mongoose.Types.ObjectId,
+		projectSlug: string,
+		historyId: mongoose.Types.ObjectId
+	): Promise<Failable.Type<boolean>> {
+		return this.updateHistoryStatus(
+			ownerId,
+			projectSlug,
+			historyId,
+			"Succeeded"
+		);
+	}
+
+	public static async failed(
+		ownerId: mongoose.Types.ObjectId,
+		projectSlug: string,
+		historyId: mongoose.Types.ObjectId
+	): Promise<Failable.Type<boolean>> {
+		return this.updateHistoryStatus(
+			ownerId,
+			projectSlug,
+			historyId,
+			"Failed"
+		);
+	}
+
+	public static async createByGitUrl(
+		ownerId: mongoose.Types.ObjectId,
+		query: ProjectComm.ImportByGitUrlQuery
+	): Promise<Failable.Type<Project.Type>> {
+		const git = simplegit(ConfigRepository.paths.projects);
+		return this.create(
+			ownerId,
+			query.name,
+			"Git",
+			async (codesDir: string, artifactsDir: string) => {
+				await git.clone(query.gitUrl, codesDir);
+			}
+		);
+	}
+
+	public static async deleteProject(
+		name: string
+	): Promise<Failable.Type<string>> {
+		const p = path.resolve(ConfigRepository.paths.projects, name);
 		try {
 			await promisify(rimraf)(p);
 			return Failable.succeed(p);
@@ -33,5 +332,4 @@ class Type {
 	}
 }
 
-const ProjectRepository = new Type();
-export default ProjectRepository;
+export default Type;
