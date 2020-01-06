56c20ba7607487d81ab2c5c2e7173747454eaabd
kk-mats <k.mats.kk@gmail.com>
2020-01-06 14:40:23 +0900

    refactor: Rename uid and validate

---
+++ src/infrastructure/model/User.ts
@@ -0,0 +1,27 @@
+import * as mongoose from "mongoose";
+import { user } from "src/constants/limitations";
+
+export type DocumentType = mongoose.Document & {
+	_id: mongoose.Types.ObjectId;
+	accountId: string;
+	password: string;
+};
+
+const Schema = new mongoose.Schema({
+	accountId: {
+		type: String,
+		unique: String,
+		required: true,
+		...user.accountId
+	},
+	password: {
+		type: String,
+		requried: true,
+		...user.password
+	}
+});
+
+export const ModelName = "User";
+export const Model = mongoose.model<DocumentType>(ModelName, Schema);
+
+export default Model;
