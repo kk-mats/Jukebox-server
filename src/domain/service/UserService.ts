b004e93b462ec38dfefe6557b68f78d1f2e3d8e0
kk-mats <k.mats.kk@gmail.com>
2020-02-07 21:23:19 +0900

    feat: Use AuthenticateUser and support undefined arguments

--- src/domain/service/UserService.ts
+++ src/domain/service/UserService.ts
@@ -1,21 +1,24 @@
 import User from "src/domain/object/entity/user/User";
 import RegisterUser from "src/domain/object/entity/user/RegisterUser";
+import AuthenticateUser from "src/domain/object/entity/user/AuthenticateUser";
 
 import UserRepository from "src/infrastructure/repository/UserRepository";
 
 class UserService {
 	public static async register(
-		accountId: string,
-		password: string
+		accountId?: string,
+		password?: string
 	): Promise<User> {
 		return UserRepository.create(new RegisterUser(accountId, password));
 	}
 
 	public static async login(
-		accountId: string,
-		cleartextPassword: string
+		accountId?: string,
+		cleartextPassword?: string
 	): Promise<User | null> {
-		return UserRepository.authenticate(accountId, cleartextPassword);
+		return UserRepository.authenticate(
+			new AuthenticateUser(accountId, cleartextPassword)
+		);
 	}
 }
 
