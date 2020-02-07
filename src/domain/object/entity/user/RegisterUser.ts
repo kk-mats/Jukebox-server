b8df6b448de3925260a58eb322616518037945fa
kk-mats <k.mats.kk@gmail.com>
2020-02-07 21:24:34 +0900

    feat: Add check on undefined arguments

--- src/domain/object/entity/user/RegisterUser.ts
+++ src/domain/object/entity/user/RegisterUser.ts
@@ -1,14 +1,26 @@
+import { missingParameter } from "src/error/common";
+
 import AccountId from "src/domain/object/value/user/AccountId";
 import CleartextPassword from "src/domain/object/value/user/CleartextPassword";
+
+import { ACCOUNT_ID, PASSWORD } from "src/constants/fieldNames";
 
 class RegisterUser {
 	readonly accountId: AccountId;
 
 	readonly password: CleartextPassword;
 
-	constructor(rawAccountId: string, cleartextPassword: string) {
-		this.accountId = new AccountId(rawAccountId);
-		this.password = new CleartextPassword(cleartextPassword);
+	constructor(rawAccountId?: string, cleartextPassword?: string) {
+		if (!rawAccountId) {
+			throw missingParameter(ACCOUNT_ID);
+		}
+
+		if (!cleartextPassword) {
+			throw missingParameter(PASSWORD);
+		}
+
+		this.accountId = new AccountId(rawAccountId, ACCOUNT_ID);
+		this.password = new CleartextPassword(cleartextPassword, PASSWORD);
 	}
 }
 
